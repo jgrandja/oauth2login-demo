f40562e8631c3ca2af204633a46a1c4d019220ec
Joe Grandja <jgrandja@pivotal.io>
2018-10-18 12:15:29 -0400

    Use WebClient oauth2Configuration

--- src/main/java/sample/web/MainController.java
+++ src/main/java/sample/web/MainController.java
@@ -15,73 +15,47 @@
  */
 package sample.web;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpHeaders;
 import org.springframework.security.oauth2.client.OAuth2AuthorizedClient;
-import org.springframework.security.oauth2.client.OAuth2AuthorizedClientService;
-import org.springframework.security.oauth2.client.authentication.OAuth2AuthenticationToken;
+import org.springframework.security.oauth2.client.annotation.RegisteredOAuth2AuthorizedClient;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.reactive.function.client.ClientRequest;
-import org.springframework.web.reactive.function.client.ExchangeFilterFunction;
 import org.springframework.web.reactive.function.client.WebClient;
-import reactor.core.publisher.Mono;
 
-import java.util.Collections;
 import java.util.Map;
+
+import static org.springframework.security.oauth2.client.web.reactive.function.client.ServletOAuth2AuthorizedClientExchangeFilterFunction.oauth2AuthorizedClient;
 
 /**
  * @author Joe Grandja
  */
 @Controller
 public class MainController {
+	private final WebClient webClient;
 
-	@Autowired
-	private OAuth2AuthorizedClientService authorizedClientService;
+	public MainController(WebClient webClient) {
+		this.webClient = webClient;
+	}
 
 	@GetMapping("/")
-	public String index(Model model, OAuth2AuthenticationToken authentication) {
-		OAuth2AuthorizedClient authorizedClient =
-				this.authorizedClientService.loadAuthorizedClient(
-						authentication.getAuthorizedClientRegistrationId(),
-						authentication.getName());
-		model.addAttribute("userName", authentication.getName());
+	public String index(Model model, @RegisteredOAuth2AuthorizedClient OAuth2AuthorizedClient authorizedClient) {
+		model.addAttribute("userName", authorizedClient.getPrincipalName());
 		model.addAttribute("clientName", authorizedClient.getClientRegistration().getClientName());
 		return "index";
 	}
 
 	@GetMapping("/userinfo")
-	public String userinfo(Model model, OAuth2AuthenticationToken authentication) {
-		OAuth2AuthorizedClient authorizedClient =
-				this.authorizedClientService.loadAuthorizedClient(
-						authentication.getAuthorizedClientRegistrationId(),
-						authentication.getName());
-		Map userAttributes = Collections.emptyMap();
+	public String userinfo(Model model, @RegisteredOAuth2AuthorizedClient OAuth2AuthorizedClient authorizedClient) {
 		String userInfoEndpointUri = authorizedClient.getClientRegistration()
-			.getProviderDetails().getUserInfoEndpoint().getUri();
-		if (!StringUtils.isEmpty(userInfoEndpointUri)) {	// userInfoEndpointUri is optional for OIDC Clients
-			userAttributes = WebClient.builder()
-				.filter(oauth2Credentials(authorizedClient))
-				.build()
+				.getProviderDetails().getUserInfoEndpoint().getUri();
+		Map userAttributes = this.webClient
 				.get()
 				.uri(userInfoEndpointUri)
+				.attributes(oauth2AuthorizedClient(authorizedClient))
 				.retrieve()
 				.bodyToMono(Map.class)
 				.block();
-		}
 		model.addAttribute("userAttributes", userAttributes);
 		return "userinfo";
-	}
-
-	private ExchangeFilterFunction oauth2Credentials(OAuth2AuthorizedClient authorizedClient) {
-		return ExchangeFilterFunction.ofRequestProcessor(
-			clientRequest -> {
-				ClientRequest authorizedRequest = ClientRequest.from(clientRequest)
-					.header(HttpHeaders.AUTHORIZATION, "Bearer " + authorizedClient.getAccessToken().getTokenValue())
-					.build();
-				return Mono.just(authorizedRequest);
-			});
 	}
 }
