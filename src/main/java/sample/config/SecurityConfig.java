76cfb189e7a9ffa481970828006a5060d240b960
Joe Grandja <jgrandja@pivotal.io>
2018-10-24 16:01:38 -0400

    Add Facebook login client

--- src/main/java/sample/config/SecurityConfig.java
+++ src/main/java/sample/config/SecurityConfig.java
@@ -15,7 +15,11 @@
  */
 package sample.config;
 
+import org.springframework.http.MediaType;
+import org.springframework.http.converter.AbstractHttpMessageConverter;
 import org.springframework.http.converter.FormHttpMessageConverter;
+import org.springframework.http.converter.HttpMessageConverter;
+import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
@@ -23,11 +27,17 @@ import org.springframework.security.oauth2.client.endpoint.DefaultAuthorizationC
 import org.springframework.security.oauth2.client.endpoint.OAuth2AccessTokenResponseClient;
 import org.springframework.security.oauth2.client.endpoint.OAuth2AuthorizationCodeGrantRequest;
 import org.springframework.security.oauth2.client.http.OAuth2ErrorResponseErrorHandler;
+import org.springframework.security.oauth2.client.userinfo.DefaultOAuth2UserService;
+import org.springframework.security.oauth2.client.userinfo.OAuth2UserRequest;
+import org.springframework.security.oauth2.client.userinfo.OAuth2UserService;
 import org.springframework.security.oauth2.core.http.converter.OAuth2AccessTokenResponseHttpMessageConverter;
+import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.web.client.RestTemplate;
 import sample.web.OAuth2AccessTokenResponseConverterWithDefaults;
 
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 
 /**
  * @author Joe Grandja
@@ -44,7 +54,10 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 				.and()
 			.oauth2Login()
 				.tokenEndpoint()
-					.accessTokenResponseClient(authorizationCodeTokenResponseClient());
+					.accessTokenResponseClient(authorizationCodeTokenResponseClient())
+					.and()
+				.userInfoEndpoint()
+					.userService(oauth2UserService());
 	}
 	// @formatter:on
 
@@ -61,5 +74,40 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 		tokenResponseClient.setRestOperations(restTemplate);
 
 		return tokenResponseClient;
+	}
+
+	private OAuth2UserService<OAuth2UserRequest, OAuth2User> oauth2UserService() {
+		RestTemplate restTemplate = new RestTemplate();
+		restTemplate.setErrorHandler(new OAuth2ErrorResponseErrorHandler());
+		enhanceJsonMessageConverter(restTemplate);
+
+		DefaultOAuth2UserService oauth2UserService = new DefaultOAuth2UserService();
+		oauth2UserService.setRestOperations(restTemplate);
+
+		return oauth2UserService;
+	}
+
+	private void enhanceJsonMessageConverter(RestTemplate restTemplate) {
+		// NOTE:
+		// Facebook's UserInfo API -> https://graph.facebook.com/me
+		// returns "text/javascript; charset=UTF-8" for the "content-type" response header
+		// even though the content is JSON. This is not correct and should be reported to Facebook to fix.
+		//
+		// This is a temporary workaround that adds "text/javascript; charset=UTF-8"
+		// as a supported MediaType in MappingJackson2HttpMessageConverter,
+		// which is used to convert the UserInfo response to a Map.
+
+		HttpMessageConverter<?> jsonMessageConverter = restTemplate.getMessageConverters().stream()
+				.filter(c -> c instanceof  MappingJackson2HttpMessageConverter)
+				.findFirst()
+				.orElse(null);
+
+		if (jsonMessageConverter == null) {
+			return;
+		}
+
+		List<MediaType> supportedMediaTypes = new ArrayList<>(jsonMessageConverter.getSupportedMediaTypes());
+		supportedMediaTypes.add(MediaType.valueOf("text/javascript;charset=UTF-8"));
+		((AbstractHttpMessageConverter) jsonMessageConverter).setSupportedMediaTypes(supportedMediaTypes);
 	}
 }
