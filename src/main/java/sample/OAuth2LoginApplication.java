249547ed1dd8e07646d580e4dfadc0d19def629e
Neha Singh <62351168+singhneha94@users.noreply.github.com>
2020-10-02 10:42:25 +0530

    Update OAuth2LoginApplication.java

--- src/main/java/sample/OAuth2LoginApplication.java
+++ src/main/java/sample/OAuth2LoginApplication.java
@@ -21,10 +21,11 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 /**
  * @author Joe Grandja
  */
-@SpringBootApplication
-public class OAuth2LoginApplication {
 
-	public static void main(String[] args) {
+@SpringBootApplication
+public class OAuth2LoginApplication{
+
+	public static void main(String[] args){
 		SpringApplication.run(OAuth2LoginApplication.class, args);
 	}
 }
